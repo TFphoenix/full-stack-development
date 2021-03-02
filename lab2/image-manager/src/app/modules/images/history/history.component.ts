ab4adc351b12b8769427b384cad39c0b944ae7e0
Teodor <teodor@mihaescu.info>
2021-03-02 02:16:08 +0200

    Finished lab2 UI implementation

--- lab2/image-manager/src/app/modules/images/history/history.component.ts
+++ lab2/image-manager/src/app/modules/images/history/history.component.ts
@@ -1,4 +1,27 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  size: number;
+  result: number;
+  download: string;
+  position: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, size: 1, name: 'Hydrogen', result: 1.0079, download: 'H' },
+  { position: 2, size: 2, name: 'Helium', result: 4.0026, download: 'He' },
+  { position: 3, size: 3, name: 'Lithium', result: 6.941, download: 'Li' },
+  { position: 4, size: 4, name: 'Beryllium', result: 9.0122, download: 'Be' },
+  { position: 5, size: 5, name: 'Boron', result: 10.811, download: 'B' },
+  { position: 6, size: 6, name: 'Carbon', result: 12.0107, download: 'C' },
+  { position: 7, size: 7, name: 'Nitrogen', result: 14.0067, download: 'N' },
+  { position: 8, size: 8, name: 'Oxygen', result: 15.9994, download: 'O' },
+  { position: 9, size: 9, name: 'Fluorine', result: 18.9984, download: 'F' },
+  { position: 10, size: 10, name: 'Neon', result: 20.1797, download: 'Ne' }
+];
 
 @Component({
   selector: 'app-history',
@@ -6,10 +29,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent implements OnInit {
+  displayedColumns: string[] = ['select', 'name', 'size', 'result', 'download'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
   }
 
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ? this.selection.clear() : this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
 }
--- lab2/image-manager/src/app/modules/images/images.module.ts
+++ lab2/image-manager/src/app/modules/images/images.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ImagesRoutingModule } from './images-routing.module';
-
+import { MaterialModule } from '../material/material.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HistoryComponent } from './history/history.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ImagesRoutingModule
-  ]
+  declarations: [HistoryComponent, UploadComponent],
+  imports: [SharedModule, ImagesRoutingModule, MaterialModule]
 })
-export class ImagesModule { }
+export class ImagesModule {}
