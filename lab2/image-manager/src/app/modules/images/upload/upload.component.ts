4bcf4590d37bd9f601738c124f61620cbae48dac
Teodor <teodor@mihaescu.info>
2021-03-02 11:49:12 +0200

    Lab2 Implemented upload image functionality

--- lab2/image-manager/src/app/modules/images/upload/upload.component.ts
+++ lab2/image-manager/src/app/modules/images/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-upload',
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent implements OnInit {
+  @ViewChild('fileInput') fileInputElement: ElementRef;
 
-  constructor() { }
+  imageUrl: string;
+  isImageUploaded: boolean;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.isImageUploaded = false;
   }
 
+  onUploadClicked(event) {
+    this.fileInputElement.nativeElement.click();
+  }
+
+  // called each time file input changes
+  onImageUpload(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      // read file as data url
+      reader.readAsDataURL(event.target.files[0]);
+
+      // called once readAsDataURL is completed
+      reader.onload = event => {
+        this.imageUrl = event.target.result.toString();
+        this.isImageUploaded = true;
+      };
+    }
+  }
 }
