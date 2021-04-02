182534b1e28bf3f4ab5a28701ec0a48c2b1efbcd
a3drian <38135936+a3drian@users.noreply.github.com>
2021-04-02 22:17:31 +0300

    Removed

--- image-manager-api-old/src/interfaces/orm/IExpressRequest.ts
+++
@@ -1,6 +0,0 @@
-import { Request } from 'express';
-import { EntityManager } from '@mikro-orm/core';
-
-export interface IExpressRequest extends Request {
-	em?: EntityManager;
-}
--- image-manager-api-old/src/models/AuthResponseData.ts
+++
@@ -1,8 +0,0 @@
-import { IAuthResponseData } from '../interfaces/IAuthResponseData';
-
-export class AuthResponseData implements IAuthResponseData {
-    email!: string;
-    id!: string;
-    token!: string;
-    expiresIn!: number;
-}
