b18f0fff2adda3721e69b30f590df757513566a7
Teodor <teodor@mihaescu.info>
2021-04-02 20:43:27 +0300

    Added tensorflow and util files

--- image-manager-api-new/src/controller/image-controller.ts
+++ image-manager-api-new/src/controller/image-controller.ts
@@ -1,7 +1,7 @@
-import { getRepository } from 'typeorm';
-import { Image } from '../entity/Image';
-import { NextFunction, Request, Response } from 'express';
-import { STATUS_CODES } from '../shared/status-codes';
+import { getRepository } from "typeorm";
+import { Image } from "../entity/Image";
+import { NextFunction, Request, Response } from "express";
+import { STATUS_CODES } from "../shared/status-codes";
 
 export class ImageController {
   private imageRepository = getRepository(Image);
@@ -12,11 +12,7 @@ export class ImageController {
   }
 
   // GET
-  async getAllImages(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Image[] | null> {
+  async getAllImages(req: Request, res: Response, next: NextFunction): Promise<Image[] | null> {
     try {
       return this.imageRepository.find();
     } catch (exception) {
@@ -25,11 +21,7 @@ export class ImageController {
   }
 
   // POST
-  async saveImages(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Image[]> {
+  async saveImages(req: Request, res: Response, next: NextFunction): Promise<Image[]> {
     try {
       const body = req.body;
       return this.imageRepository.save(body);
@@ -39,26 +31,19 @@ export class ImageController {
   }
 
   // evaluate image
-  async evaluateImage(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
-    try {
-
-      const data = new MnistData();
-      await data.load();
-      const model = await tf.loadLayersModel('file://D:/Facultate/Anul 3 Semestrul II/Fac/Fullstak/source/fullstackdevapp/backend/mlModel/myTrainedModel/model.json');
-      const testData = data.nextTestBatch(1);
-      const testxs = testData.xs.reshape([1, 28, 28, 1]);
-      const labels = testData.labels.argMax(-1);
-      const preds = model.predict(testxs);
-      testxs.dispose();
-      res.status(200).send(preds + "\n" + labels);
-
-    } catch (err) {
-      console.log(err);
-    }
-
+  async evaluateImage(req: Request, res: Response, next: NextFunction) {
+    // try {
+    //   const data = new MnistData();
+    //   await data.load();
+    //   const model = await tf.loadLayersModel('file://D:/Facultate/Anul 3 Semestrul II/Fac/Fullstak/source/fullstackdevapp/backend/mlModel/myTrainedModel/model.json');
+    //   const testData = data.nextTestBatch(1);
+    //   const testxs = testData.xs.reshape([1, 28, 28, 1]);
+    //   const labels = testData.labels.argMax(-1);
+    //   const preds = model.predict(testxs);
+    //   testxs.dispose();
+    //   res.status(200).send(preds + "\n" + labels);
+    // } catch (err) {
+    //   console.log(err);
+    // }
   }
 }
--- image-manager-api-new/src/index.ts
+++ image-manager-api-new/src/index.ts
@@ -6,7 +6,7 @@ import { STATUS_CODES } from "./shared/status-codes";
 import { Application, Request, Response, NextFunction } from "express";
 
 import { Routes } from "./routes/routes";
-import { checkBearer } from "./auth/check-bearer";
+import { checkBearer } from "./util/check-bearer";
 import { env } from "./env";
 
 const cors = require("cors");
